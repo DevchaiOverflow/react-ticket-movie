7b452dd1b359bfbe1a60eaeb4797992f412607d6
Devchai072 <devchai072@gmail.com>
2020-03-18 02:44:34 +0700

    Updates

---
+++ precache-manifest.2718a6f996986ad473a18c250ed78812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed891cecfa1b256a80c394628a6bf124",
+    "url": "/react-ticket-movie/index.html"
+  },
+  {
+    "revision": "7f5db756eb3c0a7c9283",
+    "url": "/react-ticket-movie/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a12d6b65705e68b24426",
+    "url": "/react-ticket-movie/static/js/2.957c400c.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-ticket-movie/static/js/2.957c400c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5db756eb3c0a7c9283",
+    "url": "/react-ticket-movie/static/js/main.43789600.chunk.js"
+  },
+  {
+    "revision": "41cd47125c1c6c89b0b3",
+    "url": "/react-ticket-movie/static/js/runtime-main.5bd30bf8.js"
+  }
+]);
